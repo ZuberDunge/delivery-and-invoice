4beefd2ce57c76c80da368ea13c993996193ab7c
MernRash <therahul.shrestha72744@gmail.com>
2021-11-26 23:58:56 +0530

    Minor Titlecase Change in Home Page

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <h1>This is Our Team Project named as : Delivery Invoice</h1>
+            <h1>This is Our Team Project Named as : Delivery Invoice</h1>
         </div>
     )
 }
