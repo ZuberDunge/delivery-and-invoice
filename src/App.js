dd8d5b2262bdc78656531701998ddd1485669595
paritosh251197 <paritoshkashyap25@gmail.com>
2021-11-26 23:53:24 +0530

    Adding Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './component/home/home';
+import Navbar from './component/navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
 
       <h2>Under Development</h2>
+      <Navbar />
       <Home />
     </div>
   );
---
+++ src/component/navbar/Navbar.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Navbar() {
+    return (
+       <div className="nav-container">
+          <h1>its navbar component</h1>
+       </div>
+    );
+}
+
+export default Navbar;
