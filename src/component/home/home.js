edbce75736e7b2f37d0d689571457538b193b433
MernRash <therahul.shrestha72744@gmail.com>
2021-11-29 20:55:19 +0530

    Changes From Devlopment Branch

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -4,6 +4,7 @@ function Home() {
     return (
         <div>
             <h1>This is Our Team Project Named as : Delivery Invoice</h1>
+            <h2>Chnages from the Development Branch</h2>
         </div>
     )
 }
